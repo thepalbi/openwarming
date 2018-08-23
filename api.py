4b561d8525a494d230548e0d0acdf70b56b4e785
pbalbi <pablo.balbi@mercadolibre.com>
2018-08-23 19:13:32 -0300

    get location average temps

--- api.py
+++ api.py
@@ -13,7 +13,7 @@ class UserWithoutLocation(Exception):
 
 class APIError(Exception):
     def errorMessage(self):
-        return "error_in_github_api"
+        return "error_in_api_request"
 
 def getUserLocation(anUsername):
     response = requests.get("https://api.github.com/users/" + anUsername)
@@ -51,14 +51,37 @@ def getUserReposCreationDates(anUsername):
 
     return reposCreationDates
 
+# t % curl -v 'http://api.worldweatheronline.com/premium/v1/past-weather.ashx?key=d865b95a583447a593b210600182308&q=Argentina&format=json&date=2018-04-11'|jq "."
+WEATHER_API_KEY = "d865b95a583447a593b210600182308"
+
+def getDateAverageTemperature(aDate, aLocation):
+    # Add date format
+    formattedDate = aDate
+    # URLEncode location
+    response = requests.get("http://api.worldweatheronline.com/premium/v1/past-weather.ashx" +\
+        f'?key={WEATHER_API_KEY}&q={aLocation}&format=json&date={formattedDate}')
+
+    if response.status_code != 200:
+        # TODO: Add more error description in API Error class
+        raise APIError
+
+    # .data.weather[0].maxtempC
+    # .data.weather[0].mintempC
+    weatherData = response.json()["data"]["weather"][0]
+
+    maxTemp = float(weatherData["maxtempC"])
+    minTemp = float(weatherData["mintempC"])
+
+    return (maxTemp + minTemp) / 2
+
 class TemperatureHandler(tornado.web.RequestHandler):
     def get(self, ghUser):
         userLocation = None
 
         try:
             userLocation = getUserLocation(ghUser)
-
-            print(getUserReposCreationDates(ghUser))
+            # print(getUserReposCreationDates(ghUser))
+            print(getDateAverageTemperature("2018-04-12", "Argentina"))
         except (UserNotFound, APIError, UserWithoutLocation) as e:
             self.set_status(404)
             self.write(e.errorMessage())
