453e04592bfbcfc760f759c52251e262452b16a2
pbalbi <pablo.balbi@mercadolibre.com>
2018-08-23 18:20:01 -0300

    More API error checking

--- api.py
+++ api.py
@@ -2,22 +2,27 @@ import requests
 import tornado.ioloop
 import tornado.web
 
-class MainHandler(tornado.web.RequestHandler):
+class TemperatureHandler(tornado.web.RequestHandler):
     def get(self, ghUser):
         response = requests.get("https://api.github.com/users/" + ghUser)
+        userData = response.json()
 
-        if "message" in response.json() and\
-            response.json()["message"] == "Not Found":
+        if response.status_code != 200:
+            self.set_status(404)
+            self.write("error_in_github_api")
+
+        if "message" in userData and\
+            userData["message"] == "Not Found":
             self.set_status(404)
             self.write("invalid_github_user")
             return
 
         self.set_status(200)
-        self.write(response.json()["location"])
+        self.write(userData["location"])
 
 def make_app():
     return tornado.web.Application([
-        ("/temperatures/(.*)", MainHandler)
+        ("/temperatures/(.*)", TemperatureHandler)
     ])
 
 if __name__ == "__main__":
