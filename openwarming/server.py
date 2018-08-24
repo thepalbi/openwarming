9e396d8b99ed6db3a3b74d87747c17d096d26f60
pbalbi <pablo.balbi@mercadolibre.com>
2018-08-23 21:43:29 -0300

    Fix unindented lines

--- openwarming/server.py
+++ openwarming/server.py
@@ -1,8 +1,11 @@
 from . import handlers
 import tornado.web
+import logging
 
 class Server(tornado.web.Application):
     def __init__(self, **kwargs):
         api_handlers = [
         ("/temperatures/(.*)", handlers.TemperatureHandler)
-    ]
+        ]
+        logging.debug(api_handlers)
+        super(Server, self).__init__(api_handlers, **kwargs)
