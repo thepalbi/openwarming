aa09465c13d0149d3095ad747777f08ec4701cb3
pbalbi <pablo.balbi@mercadolibre.com>
2018-08-23 22:28:51 -0300

    Indent fix

--- openwarming/server.py
+++ openwarming/server.py
@@ -5,7 +5,7 @@ import logging
 class Server(tornado.web.Application):
     def __init__(self, **kwargs):
         api_handlers = [
-        ("/temperatures/(.*)", handlers.TemperatureHandler)
+            ("/temperatures/(.*)", handlers.TemperatureHandler)
         ]
         logging.debug(api_handlers)
         super(Server, self).__init__(api_handlers, **kwargs)
